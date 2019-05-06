9c31d0e73b1fc00f079cd4c10e5f1c3140f99e4c
YaJun <502113690@qq.com>
2019-05-06 20:59:04 +0800

    add detail

---
+++ test1/data/db.js
@@ -0,0 +1,24 @@
+var articles = [
+  {
+    avatar: '../../images/swiper/s1.jpg',
+    date: '2019-5-6',
+    title: '我是标题1',
+    img: '../../images/swiper/s2.jpg',
+    desc: '我是描述',
+    star: '30',
+    view: '40'
+  },
+  {
+    avatar: '../../images/swiper/s1.jpg',
+    date: '2019-5-18',
+    title: '我是标题2',
+    img: '../../images/swiper/s2.jpg',
+    desc: '我是描述',
+    star: '50',
+    view: '45'
+  }
+];
+
+module.exports  = {
+  articles:articles
+}
--- test1/pages/article/article.js
+++ test1/pages/article/article.js
@@ -1,24 +1,76 @@
+var { articles } = require('../../data/db.js')
 Page({
-  data:{
-    articles:[
-      {
-        avatar:'../../images/swiper/s1.jpg',
-        date:'2019-5-6',
-        title:'我是标题1',
-        img:'../../images/swiper/s2.jpg',
-        desc:'我是描述',
-        start:'30',
-        view:'40'
-      },
-      {
-        avatar: '../../images/swiper/s1.jpg',
-        date: '2019-5-8',
-        title: '我是标题2',
-        img: '../../images/swiper/s2.jpg',
-        desc: '我是描述',
-        start: '50',
-        view: '45'
-      }
-    ]
+  onLoad: function (options) {
+    //console.log('article onLoad');
+
+    /*
+    this.data.articles = articles;
+    console.log(this.data)
+    */
+    /*
+    this.setData({
+      articles: articles
+    },function(){
+      console.log('in cb', this.data.articles)
+    }.bind(this))
+    console.log('out cb',this.data.articles)
+    */
+    this.setData({
+      articles: articles
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    //console.log('article onReady');
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //console.log('article onShow');
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    //console.log('article onHide');
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    //console.log('article onUnload');
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    //console.log('article onPullDownRefresh');
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    //console.log('article onReachBottom');
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    //console.log('article onShareAppMessage');
+  },
+  tapArticleItem: function () {
+    wx.navigateTo({
+      url: './article-detail/article-detail',
+    })
   }
 })
