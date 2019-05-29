7650387b3c65aa1d50b47ade14e8dacf2820331c
YaJun <502113690@qq.com>
2019-05-29 13:40:52 +0800

    add new chat

--- test1/data/db.js
+++ test1/data/db.js
@@ -6,7 +6,8 @@ var articles = [
     img: '../../images/swiper/s2.jpg',
     desc: '我是描述',
     star: '30',
-    view: '40'
+    view: '40',
+    articleId:'0'
   },
   {
     avatar: '../../images/swiper/s1.jpg',
@@ -15,8 +16,19 @@ var articles = [
     img: '../../images/swiper/s2.jpg',
     desc: '我是描述',
     star: '50',
-    view: '45'
-  }
+    view: '45',
+    articleId: '1'
+  },
+  {
+    avatar: '../../images/swiper/s1.jpg',
+    date: '2019-5-18',
+    title: '我是标题3',
+    img: '../../images/swiper/s2.jpg',
+    desc: '我是描述',
+    star: '50',
+    view: '45',
+    articleId: '2'
+  },
 ];
 
 module.exports  = {
