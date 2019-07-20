9dd38db32a7ffa399ba449856757595eb5668ed6
YaJun <502113690@qq.com>
2019-07-20 23:51:28 +0800

    add more appchart

--- test1/data/db.js
+++ test1/data/db.js
@@ -1,14 +1,19 @@
 var articles = [
   {
-    avatar: '/images/swiper/s1.jpg',
+    avatar: '/images/avatar/u1.png',
     date: '2019-5-6',
     time:'3天前',
     title: '我是标题1',
-    img: '/images/swiper/q1.png',
+    img: '/images/swiper/s1.jpg',
     desc: '我是描述',
     star: '30',
     view: '40',
-    articleId:'0'
+    articleId:'0',
+    music: {
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+      title: '此时此刻',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    }
   },
   {
     avatar: '/images/swiper/s1.jpg',
@@ -19,7 +24,12 @@ var articles = [
     desc: '我是描述',
     star: '50',
     view: '45',
-    articleId: '1'
+    articleId: '1',
+    music: {
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+      title: '此时此刻',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    } 
   },
   {
     avatar: '/images/swiper/s1.jpg',
@@ -30,7 +40,12 @@ var articles = [
     desc: '我是描述',
     star: '50',
     view: '45',
-    articleId: '2'
+    articleId: '2',
+    music: {
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+      title: '此时此刻',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    } 
   },
 ];
 
--- test1/pages/article/article-detail/article-detail.js
+++ test1/pages/article/article-detail/article-detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isPlaying:false,
   },
 
   /**
@@ -14,9 +14,94 @@ Page({
   onLoad: function (options) {
     var articleId=options.articleId;
     var article = articles[articleId];
-    this.setData({...article})
+    //处理状态
+    var articles_collection = wx.getStorageSync("articles_collection")
+    var isCollected = false;
+    if (!articles_collection){
+      var data={}
+      data[articleId]=false;
+      wx.setStorageSync("articles_collection",data)
+    }else{
+      isCollected = !!articles_collection[articleId]
+    }
+    this.setData({ ...article, isCollected: isCollected})
+    //监听音乐播放
+    var backgroundAudioManager = wx.getBackgroundAudioManager();
+    backgroundAudioManager.onPlay(function () {
+      this.setData({
+        isPlaying: true
+      })
+    }.bind(this))
+    backgroundAudioManager.onPause(function () {
+      this.setData({
+        isPlaying: false
+      })
+    }.bind(this))    
   },
+  /*
+  *处理收藏
+  */
+  tapCollect:function(){
+    // wx.setStorageSync("key1", 123);
+    // wx.setStorageSync("key2", 'hellow');
+    // wx.setStorageSync("key3", {
+    //   name:'tom'
+    // });
+    // console.log(wx.getStorageSync('key1'))
+    var articles_collection = wx.getStorageSync("articles_collection")
+    var isCollected = articles_collection[this.data.articleId];
+    //改变storage数据
+    articles_collection[this.data.articleId] = !isCollected;
+    wx.setStorageSync('articles_collection', articles_collection)
+    this.setData({
+      isCollected:!isCollected
+    },function(){
+      wx.showToast({
+        title:isCollected ?'取消成功':'收藏成功'
+      })
+    })
+  },
+  /*
+  *处理分享
+  */
+  tapShare:function(){
+    var itemList = ['分享到朋友圈', '分享到QQ', '分享到微博',]
+    wx.showActionSheet({
+      itemList: itemList,
+      success:function(res){
+        wx.showToast({
+          title: itemList[res.tapIndex]+'成功',
+        })
+      }
+    })
+  },
+  /*
+  *音乐播放
+  */
+  tapMusic:function(){
+    var backgroundAudioManager = wx.getBackgroundAudioManager();
+    var isPlaying = this.data.isPlaying;
+    if(isPlaying){
+      backgroundAudioManager.pause();
+      this.setData({
+        isPlaying: false,
 
+      })
+    }else{
+      var music = articles[this.data.articleId].music;
+      backgroundAudioManager.src = music.src;
+      
+      backgroundAudioManager.coverImgUrl = music.coverImgUrl;
+      backgroundAudioManager.title = music.title;
+      // backgroundAudioManager.play();
+      this.setData({
+        isPlaying:true,
+
+      })
+    }
+ 
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
